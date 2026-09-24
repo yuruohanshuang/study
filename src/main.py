e074fca99aae3071046c4178df8bbf5351def63a
Yu <zhangsan@example.com>
2026-09-24 18:00:36 +0800

    9.24

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 # 格式化前 ❌
-import os
+import logging
 
-import requests
-
-from src.utils import get_logger
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s", datefmt="")
 
 
 def calculate_average(numbers):
@@ -11,3 +9,21 @@ def calculate_average(numbers):
     for num in numbers:
         total += num
     return total / len(numbers)
+
+
+def divide(a, b):
+    return a / b
+
+
+list1 = [(10, 2), (8, 0), (6, 3)]
+count = 0
+for a in list1:
+    try:
+        divide(a[0], a[1])
+        count = count + 1
+    except ZeroDivisionError:
+        logging.error("除零错误: %s / %s", a[0], a[1])
+    except Exception as e:
+        logging.error("发生错误: %s", e)
+
+print(count)
